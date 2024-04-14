d6729117af591fb19414da61cd433bfa914e87ed
Sourav Bhowal <souravbhowal15@gmail.com>
2024-04-14 18:09:26 +0530

    url

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         domains: ["res.cloudinary.com"],
     },
     env: {
-        API_URL: "https://blog-site-server-d9uh.onrender.com/",
+        API_URL: "http://localhost:8000",
     }
 
 };
